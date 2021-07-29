d56db0b4f59150c5854b89b4b617877b1482137f
Jared Lundholm <lundholmj@gmail.com>
2021-07-29 11:49:19 -0600

    added input validation

--- passphrase_generator/passphrase_generator.py
+++ passphrase_generator/passphrase_generator.py
@@ -1,13 +1,27 @@
 import random
 import wordlist
 
-#variables
+# variables
 nr_password = []
 r_password = ""
+test = 32
 
-#program start
+# Input from user
 print("Welcome to the phasephrase generator!")
-nr_input = int(input("How many words would you like in your phasephrase?\n"))
+
+while True:
+    nr_input = input("How many words would you like in your phasephrase? Normally 4-6 words.\n")
+    try:
+        nr_input=int(nr_input)
+    except:
+        print("\nPlease use numeric digits.")
+        continue
+    if nr_input < 1:
+        print("\nPlease enter a positive number.")
+        continue
+    break
+
+#create a list from random words from wordlist
 nr_password.extend(random.sample(wordlist.words, nr_input))
 
 #print(nr_password)
@@ -17,5 +31,9 @@ print(nr_password)
 for char in nr_password:
     r_password += char
 
-print(f"\nYour passphrase is: {r_password}")
+if nr_input > 6:
+    print("\nYour passphrase is going to be very long and might be hard to remember.")
+
+print(f"\nYour super random secret and secure passphrase is: {r_password}")
+
 
