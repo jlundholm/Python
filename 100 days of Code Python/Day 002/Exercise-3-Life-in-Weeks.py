0970845f629ff8dee89621274af296b16cefc8ec
Jared Lundholm <lundholmj@gmail.com>
2022-04-26 14:20:12 -0600

    finished Life in Weeks and started final day 2 project

---
+++ 100 days of Code Python/Day 002/Exercise-3-Life-in-Weeks.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+days = int(age) * 365
+weeks = int(age) * 52
+months = int(age) * 12
+print(f"You have {32850 - days} days, {4680 - weeks} weeks, and {1080 - months} months left.")
