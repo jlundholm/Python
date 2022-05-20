b2642f4a7db462ffd214de59cdd53b6fb80e4674
Jared Lundholm <lundholmj@gmail.com>
2022-05-20 08:45:59 -0600

    Update Exercise-3-Leap-Year.py

--- 100 days of Code Python/Day 003/Exercise-3-Leap-Year.py
+++ 100 days of Code Python/Day 003/Exercise-3-Leap-Year.py
@@ -1,2 +1,17 @@
+#2016 Leap
+#2020 Leap
+#1999 Not Leap
+#2018 Not Leap
 # input "Which year do you want to check? "
-leap = input("Which year do you want to check? ")
+year = int(input("Which year do you want to check? "))
+
+if year % 4 != 0:
+    print("Not leap year.")
+elif year % 100 != 0:
+    print("Leap year.")
+elif year % 400 == 0:
+    print("Leap year.")
+else:
+    print("Not leap year.")
+
+      
