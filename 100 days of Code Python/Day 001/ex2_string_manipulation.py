2fa35c974c25b78f3e824b0c0c74ed567d3c6de6
Jared Lundholm <lundholmj@gmail.com>
2025-01-02 10:16:41 -0700

    Create ex2_string_manipulation.py
    finished example 2 string manipulation

---
+++ 100 days of Code Python/Day 001/ex2_string_manipulation.py
@@ -0,0 +1,7 @@
+print("Hello world!\nHello world!\nHello world!")
+print("Hello " + "Jared")
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
