134d1cec35067268036702743300ac841214efa2
Jared Lundholm <lundholmj@gmail.com>
2025-01-02 10:42:13 -0700

    Create ex4_variables.py
    finished example 4

---
+++ 100 days of Code Python/Day 001/ex4_variables.py
@@ -0,0 +1,14 @@
+# print length on 1 line
+print(len(input("What is your name?")))
+
+# print length using two variables
+username = input("What is your name?")
+length = len(username)
+print(length)
+
+swap the variables without using the words
+glass1 = "milk"
+glass2 = "juice"
+glass3 = glass1
+glass1 = glass2
+glass2 = glass3
