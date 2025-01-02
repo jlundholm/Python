6281f9729e29cc98954784812bbae2f00d12e8f1
Jared Lundholm <lundholmj@gmail.com>
2025-01-02 10:51:09 -0700

    Create ex5_project_band_name_generator.py
    finished project 1 band name generator

---
+++ 100 days of Code Python/Day 001/ex5_project_band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city = input("What is the name of the city that you grew up in?\n")
+pet = input("What is your favorite pet name?\n")
+print("You band name could be " + city + " " + pet + "!")
