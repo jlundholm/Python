95e1e74d7ea5819ce402553b7cdb3899ecdbc828
Jared Lundholm <lundholmj@gmail.com>
2025-01-03 07:25:23 -0700

    Create ex2_check_convert_data_types.py
    completed exercise 2 checking and converting data types.

---
+++ 100 days of Code Python/Day 002/ex2_check_convert_data_types.py
@@ -0,0 +1,10 @@
+len("Hello")
+# check data type
+print(type("Hello"))
+print(type(1234))
+print(type(3.14))
+print(type(True))
+
+# Convert Data Types
+# float() int() str()
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
