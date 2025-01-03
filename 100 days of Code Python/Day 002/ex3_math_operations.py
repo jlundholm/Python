4a8927799ff16775a0843cb9a35bba8cc4f8c44c
Jared Lundholm <lundholmj@gmail.com>
2025-01-03 07:44:23 -0700

    Create ex3_math_operations.py
    completed exercise 3 mathematical operations.

---
+++ 100 days of Code Python/Day 002/ex3_math_operations.py
@@ -0,0 +1,12 @@
+# PEMDAS
+# + - * / // and **
+print("My age: " + str(12))
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3)
+print(6 // 3)  # decision drops decimals
+print(2 ** 3) # 2 to the power of 3, 2*2*2
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
