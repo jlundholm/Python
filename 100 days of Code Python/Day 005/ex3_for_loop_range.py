042858114700ced69e149218af2fb07d74ef4a39
Jared Lundholm <lundholmj@gmail.com>
2025-01-11 16:57:24 -0700

    Create ex3_for_loop_range.py
    Exercise 3, using a for loop and range to add all the numbers from 1 - 100. Gauss Challenge.

---
+++ 100 days of Code Python/Day 005/ex3_for_loop_range.py
@@ -0,0 +1,13 @@
+# print 1 - 10
+for number in range(1, 11):
+    print(number)
+
+# print 1 - 10 by 3
+for number in range(1, 11, 3):
+    print(number)
+
+# Gauss Challenge sum number 1 - 100
+gauss = 0
+for number in range(1, 101):
+    gauss += number
+print(gauss)
